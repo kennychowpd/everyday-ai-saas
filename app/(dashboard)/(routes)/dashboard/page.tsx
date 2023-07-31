8cd7e95b103c354fb8d6fc0785f26624f3bc3574
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-07-30 20:01:51 -0400

    feature: implemented navbar and web sidebar

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <p>Dashboard Page (Protected)</p>
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ components/sidebar.tsx
@@ -0,0 +1,104 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Montserrat } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import {
+  Code2,
+  ImageIcon,
+  Layout,
+  MessageSquare,
+  Music,
+  Settings2,
+  Video,
+} from 'lucide-react'
+
+const montserrat = Montserrat({
+  weight: '600',
+  subsets: ['latin'],
+})
+
+const routes = [
+  {
+    label: 'Dashboard',
+    icon: Layout,
+    href: '/dashboard',
+    color: 'text-sky-500',
+  },
+  {
+    label: 'Chat',
+    icon: MessageSquare,
+    href: '/chat',
+    color: 'text-lime-500',
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    href: '/image',
+    color: 'text-purple-500',
+  },
+  {
+    label: 'Video Generation',
+    icon: Video,
+    href: '/video',
+    color: 'text-orange-500',
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    href: '/music',
+    color: 'text-red-500',
+  },
+  {
+    label: 'Code Generation',
+    icon: Code2,
+    href: '/code',
+    color: 'text-yellow-300',
+  },
+  {
+    label: 'Settings',
+    icon: Settings2,
+    href: '/settings',
+    color: 'text-gray-500',
+  },
+]
+
+const Sidebar = () => {
+  return (
+    <div className='space-y-4 py-4 flex flex-col h-full bg-gray-900 text-white'>
+      <div className='px-3 py-2 flex-1'>
+        <Link
+          href='/dashboard'
+          className='flex items-center pl-3 mb-14'>
+          <div className='relative w-10 h-10 mr-4'>
+            <Image
+              fill
+              alt='Logo'
+              src='/logo4.png'
+            />
+          </div>
+          <h1 className={cn('text-2xl font-bold', montserrat.className)}>
+            Everyday AI
+          </h1>
+        </Link>
+        <div className='space-y-1'>
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className='text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition'>
+              <div className='flex flex-1 items-center'>
+                <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
